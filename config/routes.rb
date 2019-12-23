2b5fddc652796e458c9ddbf6778726c38bf5170b
benoit bessiere <benoit.bessiere@gmail.com>
2019-12-23 09:19:47 +0100

    gem update + yarn upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  #home
+
+  get 'homebis', to: 'pages#homebis', as: :homebis
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
